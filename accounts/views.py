d9d68867d2bfb2ea50ac0e32806b384733612da1
IzmYuta <y.izumi@4nonome.com>
2022-06-05 13:51:46 +0900

    Create signup form

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
-from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,CreateView
+from .forms import SignUpForm
+from django.urls import reverse_lazy
 
-# Create your views here.
-class SignUpView(TemplateView):
+class SignUpView(CreateView):
     template_name = 'accounts/signup.html'
+    form_class = SignUpForm
+    success_url = reverse_lazy('home')# クラスベースビューでURL解決する時に使う(renderの代わり？)
+
+class WelcomeView(TemplateView):
+    template_name = 'welcome/index.html'
+
+class HomeView(TemplateView):
+    template_name = 'accounts/home.html'
