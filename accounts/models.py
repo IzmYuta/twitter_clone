012e1f752e49f2f4cfc5ff2d6ff54cd65cea0b66
IzmYuta <y.izumi@4nonome.com>
2022-06-19 17:06:24 +0900

    Add Profile

--- accounts/models.py
+++ accounts/models.py
@@ -4,5 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(max_length=254)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+
+
 #class FriendShip(models.Model):
 #    pass
