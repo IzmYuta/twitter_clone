5e311ea99d8a587d5aafe57865ada9f39834efd1
IzmYuta <y.izumi@4nonome.com>
2022-07-03 04:20:22 +0900

    Create TweetCreateView

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,15 @@
+from distutils.log import Log
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+from .forms import TweetForm
 
 # Create your views here.
+
+
+class TweetCreateView(LoginRequiredMixin, CreateView):
+    template_name = "tweets/tweet_create.html"
+    form_class = TweetForm
+    success_url = reverse_lazy("accounts:home")
