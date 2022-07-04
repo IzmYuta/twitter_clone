f2f6cdf62453e6e8477acce55ad79919d0909db4
IzmYuta <y.izumi@4nonome.com>
2022-07-04 13:03:15 +0900

    Create TweetDeleteView

--- tweets/views.py
+++ tweets/views.py
@@ -5,6 +5,7 @@ from django.views.generic import CreateView, DetailView, DeleteView
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic.edit import BaseCreateView
+from django.http import Http404
 
 from .forms import TweetForm
 from .models import Tweet
@@ -15,14 +16,6 @@ User = get_user_model()
 class TweetCreateView(LoginRequiredMixin, CreateView):
     template_name = "tweets/tweet_create.html"
     form_class = TweetForm
-
-    # def form_valid(self, form):
-    #     self.object = Tweet(user=self.request.user)
-    #     # tweet = form.save(commit=False)  # 保存せずオブジェクト生成する
-    #     # tweet.user = User.objects.get(id=self.kwargs["pk"])
-    #     # tweet.save()
-    #     self.object.save()
-    #     return super().form_valid(form)
 
     def post(self, request, *args, **kwargs):
         self.object = Tweet(user=self.request.user)
@@ -36,3 +29,17 @@ class TweetDetailView(DetailView):
     template_name = "tweets/tweet_detail.html"
     model = Tweet
     context_object_name = "tweet"
+
+
+class TweetDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    template_name = "tweets/tweet_delete.html"
+    model = Tweet
+    success_url = reverse_lazy("accounts:home")
+
+    def test_func(self):
+        if Tweet.objects.filter(pk=self.kwargs["pk"]).exists():
+            current_user = self.request.user
+            tweet_user = Tweet.objects.get(pk=self.kwargs["pk"]).user
+            return current_user.pk == tweet_user.pk
+        else:
+            raise Http404
